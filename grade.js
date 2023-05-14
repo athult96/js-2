9045e64394cb1806248ecd936d1de42da4e25ace
athult96 <athulorbiz@gamil.com>
2023-05-14 14:48:39 +0530

    first commit

---
+++ grade.js
@@ -0,0 +1,14 @@
+let mark=78
+
+if(mark>90){
+    console.log('Grade A')
+}
+else if(mark>70){
+    console.log("Grade B")
+}
+else if(mark>50){
+    console.log("Grade C")
+}
+else if (mark>30){
+    console.log("Grade D")
+}
---
+++ multiplication.js
@@ -0,0 +1,5 @@
+let a=6;
+
+for (i=1;i<=10;i++){
+    console.log(a,"*",i,"=",a*i)
+}
